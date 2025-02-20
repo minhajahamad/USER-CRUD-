68ab561d7a514cc322b48f6585d97c32c4bb8e16
minhajahamad <minhajahamadmk@gmail.com>
2025-02-20 16:59:22 +0530

    User Route

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const User = require('../db/model/userSchema');
+
+const router = express.Router();
+
+router.get('/');
+router.post('/');
+
+module.exports = router;
